14141d16a772f6229da2189f56d3ab9707230d65
DAVID MERCADO JUANES <dmjuanes_@abc.gob.bo>
2023-04-10 22:04:06 -0400

    [UPDATE]CONFIG NEXT HEADER AND GOOGLE ANALYTICS

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,4 +1,5 @@
-import Header from "@components/Header";
+import Header from '@components/Header';
+import Script from 'next/script';
 import AppContext from '@context/AppContext';
 import useInitialState from '@hooks/useInitialState';
 import '@styles/globals.css';
@@ -7,8 +8,17 @@ export default function App({ Component, pageProps }) {
   const initialState = useInitialState();
   return (
     <AppContext.Provider value={initialState}>
-    <Header />
+      <Script async src="https://www.googletagmanager.com/gtag/js?id=G-T0Y0RNNMM4"></Script>
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.datalayer = window.datalayer || [];
+          function gtag() {datalayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-T0Y0RNNMM4');
+        `}
+      </Script>
+      <Header />
       <Component {...pageProps} />
     </AppContext.Provider>
-  )
+  );
 }
--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,23 +1,15 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
     <Html>
       <Head>
-        <title>React Shop</title>
-        <script async src='https://www.googletagmanager.com/gtag/js?id=G-T0Y0RNNMM4'></script>
-        <script dangerouslySetInnerHTML={{ 
-          __html: `window.datalayer = window.datalayer || [];
-          function gtag() {datalayer.push(arguments);}
-          gtag('js', new Date());
-          gtag('config', 'G-T0Y0RNNMM4')
-          `
-         }} />
+        <meta charSet="utf-8" />
       </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
